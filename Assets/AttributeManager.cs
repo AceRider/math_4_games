5de507f4499159a949dfc800b88eb5bfe343c37d
Aecio Jorge <aecio.jorge@gmail.com>
2019-12-04 22:54:32 -0400

    2.9

--- Assets/AttributeManager.cs
+++ Assets/AttributeManager.cs
@@ -6,13 +6,15 @@ using System;
 
 public class AttributeManager : MonoBehaviour
 {
-    public static int MAGIC = 16;
-    public static int FLY = 18;
-    public static int CHARISMA = 20;
-    public static int INTELLIGENCE = 22;
-    public static int INVISIBLE = 24;
+    static public int MAGIC = 16;
+    static public int INTELLIGENCE = 8;
+    static public int CHARISMA = 4;
+    static public int FLY = 2;
+    static public int INVISIBLE = 1;
+
+
     public Text attributeDisplay;
-    int attributes = 0;
+    public int attributes = 0;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -20,29 +22,53 @@ public class AttributeManager : MonoBehaviour
         {
             attributes |= MAGIC;
         }
-        else if (other.gameObject.tag == "FLY")
+        else if (other.gameObject.tag == "INTELLIGENCE")
         {
-            attributes |= FLY;
+            attributes |= INTELLIGENCE;
         }
         else if (other.gameObject.tag == "CHARISMA")
         {
             attributes |= CHARISMA;
         }
-        else if (other.gameObject.tag == "INTELLIGENCE")
+        else if (other.gameObject.tag == "FLY")
         {
-            attributes |= INTELLIGENCE;
+            attributes |= FLY;
         }
         else if (other.gameObject.tag == "INVISIBLE")
         {
             attributes |= INVISIBLE;
         }
+        else if (other.gameObject.tag == "ANTIMAGIC")
+        {
+            attributes &= ~MAGIC;
+        }
+        else if (other.gameObject.tag == "REMOVE")
+        {
+            attributes &= ~ (INTELLIGENCE | MAGIC);
+        }
+        else if (other.gameObject.tag == "ADD")
+        {
+            attributes |= (INTELLIGENCE | MAGIC | CHARISMA);
+        }
+        else if (other.gameObject.tag == "RESET")
+        {
+            attributes = 0;
+        }
+
     }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(this.transform.position);
         attributeDisplay.transform.position = screenPoint + new Vector3(0,-50,0);
-        attributeDisplay.text = Convert.ToString(attributes, 2).PadLeft(0, '0');
+        attributeDisplay.text = Convert.ToString(attributes, 2).PadLeft(8, '0');
     }
        
 }
